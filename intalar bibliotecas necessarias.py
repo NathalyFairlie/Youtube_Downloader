7ad9cc965bcacddcbb92200e37d0b68f19325e18
Nathaly Fairlie Pearson Freitas <118907797+NathalyFairlie@users.noreply.github.com>
2026-07-09 09:41:38 -0300

    Update intalar bibliotecas necessarias.py

--- intalar bibliotecas necessarias.py
+++ intalar bibliotecas necessarias.py
@@ -1,6 +1,11 @@
 import subprocess
 import sys
 
+# Adiciona a pasta oculta onde o pip instalou o yt-dlp
+sys.path.append(r"C:\Users\Administrator\AppData\Roaming\Python\Python312\site-packages")
+
+import yt_dlp  # Agora ele vai encontrar sem problemas!
+
 # List of required libraries
 required_libraries = ['tkinter', 'yt-dlp', 'audioclipextractor', 'lameenc']
 
